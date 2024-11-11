dd61736c08f753596ac7f580cca1dc25fb2eb59a
Willie Hurst III <hurst.willi3@yahoo.com>
2024-11-11 08:17:11 -0600

    Update app.js code

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 'use strict';
 
-const switcher = document.querySelector('.btn');
+const body = document.body;
+const themeButton = document.querySelector('.btn');
 
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+if (themeButton) {
+    themeButton.addEventListener('click', toggleTheme);
+} else {
+    console.error('Theme switch button not found');
+}
 
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
+function toggleTheme() {
+    body.classList.toggle('light-theme');
+    body.classList.toggle('dark-theme');
+
+    const className = body.className;
+    this.textContent = className === "light-theme" ? "Dark" : "Light";
 
     console.log('current class name: ' + className);
-});
+}
